a649b7c2b7c2ed1b71126d346e9b70147960378f
arnie b <arnold.bwaila@gmail.com>
2019-08-22 10:38:30 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,9 @@
 # Write your code here.
+katz_deli = []
+ 
+  def take_a_number(array, name)
+    array << name
+    number = array.length
+    message = "Welcome, #{name}. You are number #{number} in the line"
+    message
+  end
